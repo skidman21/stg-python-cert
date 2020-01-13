61ad7ea9992aae987afb07d4291c02da63c85cb0
Shirley Kidman <shirley.kidman@gmail.com>
2020-01-12 22:51:22 -0700

    Challenge 2 Completed

--- conftest.py
+++ conftest.py
@@ -1,12 +1,18 @@
 # File to hold global pytest fixtures
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 @pytest.fixture
 def driver():
     # before each test
-    web_driver = webdriver.Chrome()
+    web_driver = webdriver.Chrome('../chromedriver')
     yield web_driver
     # after each test
     web_driver.quit()
+
+
+@pytest.fixture
+def wait(driver):
+    yield WebDriverWait(driver, timeout=10)
